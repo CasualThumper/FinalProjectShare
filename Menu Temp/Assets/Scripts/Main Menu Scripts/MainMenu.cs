c21c48d0c20a63244c84ae1ff3cd81e65b404e7e
CasualThumper <alexshumaker@outlook.com>
2025-03-04 09:56:21 -0500

    ele menu transfer

--- Menu Temp/Assets/Scripts/Main Menu Scripts/MainMenu.cs
+++ Menu Temp/Assets/Scripts/Main Menu Scripts/MainMenu.cs
@@ -1,6 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
-public class NewGame
-{
+public class MainMenu : MonoBehaviour {
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("PreviousLevel");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LevelSelectionMenu");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting...");
+        // Temporary
+        Application.Quit();
+    }
     
 }
---
+++ Menu Temp/Assets/Scripts/Main Menu Scripts/SettingsMenu.cs
@@ -0,0 +1,84 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public GameObject graphicsCanvas;
+    public GameObject optionsCanvas;
+    public Toggle fullscreen;
+    private bool fullScreenVal = false;
+    private int res = 0;
+    public Camera camera;
+    public GameObject settingsCanvas;
+    public PauseMenuScript pauseMenuScript;
+    public TMP_Dropdown dropdown;
+
+    private void Start()
+    {
+        if (camera != null)
+        {
+            Vector3 v = camera.transform.position;
+            settingsCanvas.transform.position = v;
+            settingsCanvas.transform.LookAt(v);
+            settingsCanvas.SetActive(false);
+        }
+    }
+
+    private void Update() { }
+
+    public void OpenGraphics()
+    {
+        graphicsCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+    }
+
+    public void Back()
+    {
+        optionsCanvas.SetActive(true);
+        // Set all other Canvases to false
+        graphicsCanvas.SetActive(false);
+    }
+
+    public void SetfullscreenVal()
+    {
+        this.fullScreenVal = !this.fullScreenVal;
+        Debug.Log(fullScreenVal);
+    }
+
+    public void SetRes()
+    {
+        res = dropdown.value;
+    }
+
+    public void SaveChanges()
+    {
+        if(res == 0)
+        {
+            Screen.SetResolution(1920, 1080, fullScreenVal);
+            Debug.Log(0);
+        }
+        else if(res == 1){
+            Screen.SetResolution(1280, 720, fullScreenVal);
+            Debug.Log(1);
+        }
+    }
+
+    public void Exit()
+    {
+        if (settingsCanvas != null)
+        {
+            settingsCanvas.SetActive(false);
+            pauseMenuScript.pauseCanvas.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+}
